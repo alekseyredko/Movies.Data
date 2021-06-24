904d5a7e4a70472b94d94cf56d88f0c8ec31dc13
alekseyredko <aliakseiredzko@gmail.com>
2021-06-24 11:58:07 +0300

    Initial commit

---
+++ Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using MoviesDataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesDataLayer.Interfaces
+{
+    interface IUnitOfWork: IDisposable
+    {
+        IGenericRepository<Actor> Actors { get; }
+        IGenericRepository<Genre> Genres { get; }
+        IGenericRepository<Movie> Movies { get; }
+        IGenericRepository<Person> Persons { get; }
+        IGenericRepository<Review> Reviews { get; }
+        IGenericRepository<Reviewer> Reviewers { get; }
+        IGenericRepository<ReviewerWatchHistory> ReviewersWatchHistory { get; }
+        Task<int> SaveAsync();
+    }
+}
---
+++ UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using MoviesDataLayer.Interfaces;
+using MoviesDataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoviesDataLayer
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        private MoviesDBContext _moviesDBContext;
+
+        public UnitOfWork(MoviesDBContext moviesDBContext)
+        {
+            _moviesDBContext = moviesDBContext;
+        }
+
+        public IGenericRepository<Actor> Actors => throw new NotImplementedException();
+
+        public IGenericRepository<Genre> Genres => throw new NotImplementedException();
+
+        public IGenericRepository<Movie> Movies => throw new NotImplementedException();
+
+        public IGenericRepository<Person> Persons => throw new NotImplementedException();
+
+        public IGenericRepository<Review> Reviews => throw new NotImplementedException();
+
+        public IGenericRepository<Reviewer> Reviewers => throw new NotImplementedException();
+
+        public IGenericRepository<ReviewerWatchHistory> ReviewersWatchHistory => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            _moviesDBContext.Dispose();
+        }
+
+        public Task<int> SaveAsync()
+        {
+            return _moviesDBContext.SaveChangesAsync();
+        }
+    }
+}
