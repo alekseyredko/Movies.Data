965874f3b92c73397d6e9da4be1a5618999e06f9
alekseyredko <aliakseiredzko@gmail.com>
2021-06-25 11:18:48 +0300

    add dto mapper

--- Dto/ActorDto.cs
+++ Dto/ActorDto.cs
@@ -8,5 +8,15 @@ namespace Movies.Data.Dto
 {
     public class ActorDto
     {
+        public ActorDto()
+        {
+            MoviesActors = new HashSet<MovieDto>();
+        }
+
+        public int ActorId { get; set; }
+        public int? Age { get; set; }
+
+        public virtual PersonDto ActorNavigation { get; set; }
+        public virtual ICollection<MovieDto> MoviesActors { get; set; }
     }
 }
--- Mapper/AppMappingProfile.cs
+++ Mapper/AppMappingProfile.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using Movies.Data.Dto;
+using MoviesDataLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,31 @@ using System.Threading.Tasks;
 
 namespace Movies.Data.Mapper
 {
-    class AppMappingProfile
+    class AppMappingProfile: Profile
     {
+        public AppMappingProfile()
+        {
+            CreateMap<Actor, ActorDto>()
+                .ForMember(destination => destination.MoviesActors,
+                options => options.MapFrom(src => src.MoviesActors.Select(actors => actors.Actor)));
+
+            //CreateMap<ActorDto, Actor>()
+            //    .ForMember(destination => destination.
+            //    options => options.MapFrom(src => src.MoviesActors));
+
+
+            //CreateMap<ADTO, A>()
+            //.ForMember(dest => dest.AName, opt => opt.MapFrom(src => src.AName))
+            //.ForMember(dest => dest.ABs, opt => opt.MapFrom(src => src.Bs))
+            //.AfterMap((src, dest) => {
+            //    foreach (var b in dest.ABs)
+            //    {
+            //        b.AId = src.Id;
+            //    }
+            //});
+            //CreateMap<B, AB>()
+            //          .ForMember(dest => dest.BId, opt => opt.MapFrom(src => src.Id))
+            //          .ForMember(dest => dest.B, opt => opt.MapFrom(src => src));
+        }
     }
 }
