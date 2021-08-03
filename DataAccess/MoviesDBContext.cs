dcf6653fe7fc2de87b3cacc13fae9829a4b8ed3e
alekseyredko <aliakseiredzko@gmail.com>
2021-08-03 10:23:02 +0300

    store config in json

--- DataAccess/MoviesDBContext.cs
+++ DataAccess/MoviesDBContext.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Movies.Data.Models;
 
@@ -32,8 +32,12 @@ namespace Movies.Data.DataAccess
             optionsBuilder.LogTo(Console.WriteLine, LogLevel.Trace);
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=MSQL-03467;Initial Catalog=MoviesDB;Integrated Security=True");
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                    .AddJsonFile("settings.json")
+                    .Build();
+                
+                optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
             }
         }
 
